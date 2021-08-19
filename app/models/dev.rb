aa94722851a209b78879a14315e007acdaec6220
ellijs <ellijs@hotmail.com>
2021-08-19 14:01:07 -0400

    Completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,13 +3,16 @@ class Dev < ActiveRecord::Base
     has_many :companies, through: :freebies
 
     def received_one?(item_name)
-        a = self.freebies.find_by(item_name: item_name)
-        a ? true : false
+        self.freebies.where(item_name: item_name).exists?
+        # a = self.freebies.find_by(item_name: item_name)
+        # a ? true : false
     end
 
     def give_away(dev, freebie)
-        a = self.freebies.find(freebie.id)
-        a.update(dev_id: dev.id)
+        # a = self.freebies.find(freebie.id)
+        # a.update(dev_id: dev.id)
+
+        freebie.update(dev_id: dev.id)
     end
 end
 
