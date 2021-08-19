8e372c21ac3362dff0089395bc2d1cb1fe15006a
ellijs <ellijs@hotmail.com>
2021-08-19 12:01:20 -0400

    Completed Assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,14 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def self.oldest_company
+        # self.all.sort{|a, b| a.founding_year <=> b.founding_year}.first
+        self.where.("founding_year = ? ", self.minimum(:founding_year)).first
+       
+    end
+
+    def give_freebie(dev, item_name, value)
+        self.freebies.create(item_name: item_name, value: value, company_id: self.id, dev_id: dev.id)
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        a = self.freebies.find_by(item_name: item_name)
+        a ? true : false
+    end
+
+    def give_away(dev, freebie)
+        a = self.freebies.find(freebie.id)
+        a.update(dev_id: dev.id)
+    end
 end
+
